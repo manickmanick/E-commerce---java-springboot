28f8c32ee92decdeb93d3c4f8f281a6d08c552d0
manickmanick <manickaperumal22@gmail.com>
2026-09-21 13:08:50 +0530

    feat(user-service): add JWT validation with Spring Security

--- user-service/src/main/java/com/ecommerce/user_service/config/SecurityConfig.java
+++ user-service/src/main/java/com/ecommerce/user_service/config/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.ecommerce.user_service.config;
 
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class SecurityConfig {
@@ -23,11 +30,25 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/api/users/login").permitAll()
-                                .requestMatchers("/api/users/**").permitAll()
-                                .anyRequest().permitAll()
-                );
+                                .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
 
         return http.build();
+    }
+
+    @Bean
+    public SecretKey jwtSecretKey(@Value("${jwt.secret}") String secret){
+        return Keys.hmacShaKeyFor(
+                secret.getBytes(StandardCharsets.UTF_8)
+        );
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(SecretKey jwtSecretKey) {
+        return NimbusJwtDecoder
+                .withSecretKey(jwtSecretKey)
+                .build();
     }
 
 }
